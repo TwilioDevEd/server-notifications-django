de468a4c44718eb5fc57e6b0b341ae9f7a064c95
Samuel Mendes <samuel.mendes@gmail.com>
2019-09-03 16:20:39 +0200

    Bring back media_url example parameter

--- twilio_notifications/middleware.py
+++ twilio_notifications/middleware.py
@@ -59,7 +59,10 @@ class MessageClient:
 
     def send_message(self, body, to):
         self.twilio_client.messages.create(
-            body=body, to=to, from_=self.twilio_number
+            body=body,
+            to=to,
+            from_=self.twilio_number,
+            # media_url=['https://demo.twilio.com/owl.png']
         )
 
 
